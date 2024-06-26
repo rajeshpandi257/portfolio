0ba8c1ee1d176e8935023127f15e7418dc7fc59b
Rajesh kannan pandi <108587861+rajeshpandi257@users.noreply.github.com>
2024-06-26 20:01:43 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -80,56 +80,7 @@ const Header = () => {
       </nav>
     </header>
 
-    //     <header>
-    //     <div class="header">
-    //        <div class="container">
-    //           <div class="row">
-    //              <div class="col-xl-5 col-lg-5 col-md-9 col-sm-9">
-    //                 <nav class="navigation navbar navbar-expand-md navbar-dark ">
-    //                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-    //                    <span class="navbar-toggler-icon"></span>
-    //                    </button>
-    //                    <div class="collapse navbar-collapse" id="navbarsExample04">
-    //                       <ul class="navbar-nav mr-auto">
-    //                          <li class="nav-item active">
-    //                             <a class="nav-link" href="index.html"> Home  </a>
-    //                          </li>
-    //                          <li class="nav-item">
-    //                             <a class="nav-link" href="#about">About</a>
-    //                          </li>
-    //                          <li class="nav-item">
-    //                             <a class="nav-link" href="#service">Service</a>
-    //                          </li>
-    //                          <li class="nav-item">
-    //                             <a class="nav-link" href="#customer">Customer</a>
-    //                          </li>
-    //                          <li class="nav-item">
-    //                             <a class="nav-link" href="#contact">Contact Us</a>
-    //                          </li>
-    //                       </ul>
-    //                    </div>
-    //                 </nav>
-    //              </div>
-    //              <div class="col-xl-2 col-lg-2 col-md-3 col-sm-3 col logo_section">
-    //                 <div class="full">
-    //                    <div class="center-desk">
-    //                       <div class="logo">
-    //                          <a href="index.html"><img src="images/logo.png" alt="#" /></a>
-    //                       </div>
-    //                    </div>
-    //                 </div>
-    //              </div>
-    //              <div class="col-xl-5 col-lg-5 col-md-5 col-sm-5">
-    //                 <ul class="email">
-    //                    <li><a href="#">Call: (+71) 1234567890</a></li>
-    //                    <li><a href="#">Email: demo@gmail.com</a></li>
-    //                    <li><a href="#"><i class="fa fa-search" aria-hidden="true"></i></a></li>
-    //                 </ul>
-    //              </div>
-    //           </div>
-    //        </div>
-    //     </div>
-    //  </header>
+    
   );
 };
 
